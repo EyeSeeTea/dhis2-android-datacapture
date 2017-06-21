a1bbf35b182bc1a15fa8ea3591d692a81af03379
ifoche <nacho.foche@gmail.com>
2017-06-21 12:14:05 +0200

    add test code coverage with codecov and its buddybuild integration

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bin/env bash
 
 # Definitions
 gitPath=$(git rev-parse --show-toplevel)
 
 # Generate last commit
 sh ${gitPath}/generate_last_commit.sh
+
+echo "Generate Test Coverage Report:"
+./gradlew build jacocoTestReport assembleAndroidTest
